b6dae5962bd7527e9a2380ceb59379c3ad03cce3
HouZhiping <657244927@qq.com>
2021-01-29 22:40:35 +0800

    create priject

---
+++ cd/src/test/java/com/hou/cd/CdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hou.cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
